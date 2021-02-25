d10521a50ccc660c1dc472eb77b07ecc88d79171
Łukasz Sławęta <lukasz.slaweta@microsoft.wsei.edu.pl>
2021-02-25 20:40:45 +0100

    added more books to db

--- project-cs-library/seed-data.sql
+++ project-cs-library/seed-data.sql
@@ -3,14 +3,25 @@
 SET IDENTITY_INSERT autor on;
 INSERT INTO autor(autor_id, imie, nazwisko) VALUES 
 	(1, 'Stephen', 'King'),
-	(2, 'Juliusz', 'Słowacki');
+	(2, 'Juliusz', 'Słowacki'),
+	(3, 'Ernst', 'Gombrich'),
+	(4, 'George', 'Orwell'),
+	(5, 'Olga', 'Tokarczuk'),
+	(6, 'Carl', 'Sagan');
 SET IDENTITY_INSERT autor off;
 	
 SET IDENTITY_INSERT ksiazka on;
 INSERT INTO ksiazka(ksiazka_id, autor_id, tytul, data_wydania, wydawnictwo, liczba_stron) VALUES
 	(1, 1, 'Instynkt', '2019-01-01', 'Albatros', 672),
 	(2, 1, 'Outsider', '2018-05-22', 'Albatros', 560),
-	(3, 2, 'Kordian', '1834-01-01', 'Czarne', 128);
+	(3, 2, 'Kordian', '1834-01-01', 'Czarne', 128),
+	(4, 3, 'O Sztuce', '1950-01-01', 'Czarne', 600),
+	(5, 4, 'Folwark Zwierzęcy', '1947-01-01', 'Czarne', 120),
+	(6, 4, 'Rok 1984', '1953-01-01', 'Czarne', 328),
+	(7, 5, 'Bieguni', '2007-01-01', 'Czarne', 416),
+	(8, 1, 'Krótka Historia Czasu', '1988-01-01', 'Czarne', 256),
+	(9, 1, 'Krótkie Odpowiedzi Na Wielkie Pytania', '2018-01-01', 'Czarne', 128),
+	(10, 6, 'Błękitna Kropka', '1994-01-01', 'Czarne', 429);
 SET IDENTITY_INSERT ksiazka off;
 
 SET IDENTITY_INSERT klient on;
